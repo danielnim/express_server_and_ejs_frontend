ac28682214098c4825313cc3848a1c31162faf25
danielnim <xidirovdiyorbek6@gmail.com>
2025-12-23 20:30:31 +0900

    ejs da frontend qildik

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+console.log("Express ishga tushdi");
+
 const express = require("express");
 const http = require("http");
 const app = express();
@@ -9,18 +11,27 @@ app.use(express.urlencoded({ extended: true }));
 //2: Session code
 //3: Views code
 app.set("views", "views");
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");
 //4: Routing code
-app.get('/',function(req,res){
-    res.end("Hello World")
-})
-app.get('/gift',function(req,res){
-    res.end("Siz sovgalar bolimidasiz")
-})
+// app.get('/',function(req,res){
+//     res.end("Hello World")
+// })
+// app.get('/gift',function(req,res){
+//     res.end("Siz sovgalar bolimidasiz")
+// })
+
+app.post("/create-item", function (req, res) {
+  console.log(req.body);
+  res.json({value:req.body.item})
+  
+});
+
+app.get("/", function (req, res) {
+  res.render("harid");
+});
 
 const PORT = 3000;
-const server = http.createServer(app)
-app.listen(PORT,function(){
-    console.log(`Server ishga tushdi http://localhost:${PORT}`);
-    
-})
+const server = http.createServer(app);
+server.listen(PORT, function () {
+  console.log(`Server ishga tushdi http://localhost:${PORT}`);
+});
